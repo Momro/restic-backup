0ef7fcc5b59e27953512da20509a12bdbab956ba
Momro <68013667+Momro@users.noreply.github.com>
2026-02-24 10:40:09 +0100

    Update backup.sh
    mit kuma integration

--- backup.sh
+++ backup.sh
@@ -6,22 +6,54 @@ MOUNT_POINT='/mnt/restic-restore'
 export RESTIC_REPOSITORY='/mnt/restic-backup-target'
 export RESTIC_PASSWORD='enter your password here'
 
+KUMA_PUSH_URL="http://<kuma ip>:<port>/api/push/<token>?status=up&msg=backup&ping=1234"
+KUMA_ENABLED=1 # 1 == False, 0 == True
+
+START=$(date '+%s')
+
+function isMounted {
+        if [[ $(mount | grep $RESTIC_REPOSITORY | wc -l) > 0 ]] ; then
+                return 0
+        else
+                return 1
+        fi
+}
+
 case "$1" in
         "do-backup")
-                /usr/bin/restic \
-                --verbose \
-                backup \
-                --exclude-file=$EXCLUDE_FILE \
-                --files-from=$INCLUDE_FILE
+                if isMounted ; then
+                        /usr/bin/restic \
+                        --verbose \
+                        backup \
+                        --exclude-file=$EXCLUDE_FILE \
+                        --files-from=$INCLUDE_FILE
+
+                        END=$(date '+%s')
+                        DURATION=$((END - START))
+
+                        if [[ $KUMA_ENABLED == 0 ]] ; then
+                                curl -s -o /dev/null "$KUMA_PUSH_URL"
+                        else
+                                echo "Backup finished in ${DURATION} seconds."
+                        fi
+                else
+                        echo "there is no mount at ${RESTIC_REPOSITORY}"
+                        exit 1
+                fi
         ;;
         "do-forget")
-                /usr/bin/restic \
-                forget \
-                --prune \
-                --keep-daily 30 \
-                --keep-weekly 10 \
-                --keep-monthly 12 \
-                --keep-yearly 75
+                if isMounted ; then
+                        /usr/bin/restic \
+                        forget \
+                        --prune \
+                        --keep-daily 30 \
+                        --keep-weekly 10 \
+                        --keep-monthly 12 \
+                        --keep-yearly 75
+                else
+                        echo "there is no mount at ${RESTIC_REPOSITORY}"
+                        exit 1
+                fi
         ;;
         "do-mount")
                 /usr/bin/restic \
