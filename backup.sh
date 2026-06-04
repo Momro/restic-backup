f5a35b617502e1e2581877d9461fdd42934c4991
Momro <68013667+Momro@users.noreply.github.com>
2026-06-04 18:42:28 +0200

    Update backup.sh
    formating, some more feedback and echos

--- backup.sh
+++ backup.sh
@@ -3,14 +3,55 @@ INCLUDE_FILE='/root/restic/include.txt'
 EXCLUDE_FILE='/root/restic/exclude.txt'
 MOUNT_POINT='/mnt/restic-restore'
 
-export RESTIC_REPOSITORY='/mnt/restic-backup-target'
+#####################################
+# TARGET DEFINITION
+# can either be SFTP or SMB
 
+## SFTP details
+SFTP_USER="mySftpUser"
+SFTP_TARGETNAME="resticBackupTarget"
+SFTP_REPOSITORY_MOUNTPOINT="/media/backup-usb-drive" # the remote mount point where the backup shall be stored
+RESTIC_REPOSITORY="sftp:${SFTP_USER}@${SFTP_TARGETNAME}:${SFTP_REPOSITORY_MOUNTPOINT}"
+
+## SMB mount point
+RESTIC_REPOSITORY='/mnt/restic-backup-target'
+
+export RESTIC_REPOSITORY
+#####################################
+
+#####################################
+# KEEPERS
+# How long do you want each backup to remain?
+KP_DAILY=7
+KP_WEEKLY=5
+KP_MONTHLY=12
+KP_YEARLY=3
+#####################################
+
+#####################################
+# KUMA REPORTING
+# set to 1 to enable
 KUMA_ENABLED=0
+## if the URL contains an "OK", the script will insert there the time it took to take the backup
+KUMA_PUSH_URL_FILE="/root/restic/kuma_push_url"
+#####################################
+
+#####################################
+# DO NOT CHANGE ANYTHING BELOW THIS #
+#####################################
 
 START=$(date '+%s')
 
 function isMounted {
-        if [[ $(mount | grep $RESTIC_REPOSITORY | wc -l) > 0 ]] ; then
+        # if remote is SFTP
+        if [[ $(echo $RESTIC_REPOSITORY | cut -d ":" -f 1) == "sftp" ]] ; then
+                # set default to "not mounted"
+                isMountedRemotely=1
+                # see if drive is mounted on remote host
+                isMountedRemotely=$(ssh ${SFTP_TARGETNAME} "mountpoint -q ${SFTP_REPOSITORY_MOUNTPOINT} && echo '0'")
+                # return 1 (not mounted) or 0 (mounted)
+                return $isMountedRemotely
+        elif [[ $(mount | grep $RESTIC_REPOSITORY | wc -l) > 0 ]] ; then        
                 return 0
         else
                 return 1
@@ -18,22 +59,29 @@ function isMounted {
 }
 
 function performBackup {
+        echo "[#] #####################################"
+        echo "[+] Backup to ${RESTIC_REPOSITORY}"
+        echo "[#] #####################################"
         /usr/bin/restic \
         --verbose \
         --password-file=/root/restic/.restic-pass \
-        backup \
         --exclude-file=$EXCLUDE_FILE \
-        --files-from=$INCLUDE_FILE
+        --files-from=$INCLUDE_FILE \
+        backup
 }
 
 function performPurge {
+        echo "[#] #####################################"
+        echo "[+] Purging now"
         /usr/bin/restic \
-        forget \
         --prune \
-        --keep-daily 30 \
-        --keep-weekly 10 \
-        --keep-monthly 12 \
-        --keep-yearly 75
+        --keep-daily $KP_DAILY \
+        --keep-weekly $KP_WEEKLY \
+        --keep-monthly $KP_MONTHLY \
+        --keep-yearly $KP_YEARLY
+        forget
+        echo "[#] #####################################"
+        echo "[+] Purge is done"
 }
 
 case "$1" in
@@ -44,11 +92,14 @@ case "$1" in
                         END=$(date '+%s')
                         DURATION=$((END - START))
 
-                        if [[ $KUMA_ENABLED == 0 ]] ; then
-                                curl -s -o /dev/null "$(cat ./kuma_push_url)"
+                        if [[ $KUMA_ENABLED == 1 ]] ; then
+                                curl -s -o /dev/null "$(cat ${KUMA_PUSH_URL_FILE} | sed "s/OK/${DURATION}/g" )"
+                                echo "kuma sent"
                         fi
+                        echo "[#] #####################################"
                         echo "Backup finished in ${DURATION} seconds."
                 else
+                        echo "[#] #####################################"
                         echo "there is no mount at ${RESTIC_REPOSITORY}"
                         exit 1
                 fi
@@ -57,6 +108,7 @@ case "$1" in
                 if isMounted ; then
                         performPurge
                 else
+                        echo "[#] #####################################"
                         echo "there is no mount at ${RESTIC_REPOSITORY}"
                         exit 1
                 fi
